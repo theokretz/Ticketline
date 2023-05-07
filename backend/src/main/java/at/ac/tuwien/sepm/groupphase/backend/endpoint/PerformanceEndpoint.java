6355f291e2e8b966bff89bc3e11dbe2ca5425192
Theo Kretz <e52004057@student.tuwien.ac.at>
2023-05-07 21:46:04 +0200

    fixed failed pipeline

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/PerformanceEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/PerformanceEndpoint.java
@@ -6,10 +6,12 @@ import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.UserDto;
 import at.ac.tuwien.sepm.groupphase.backend.service.PerformanceService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.lang.invoke.MethodHandles;
+
 
 @RestController
 public class PerformanceEndpoint {
@@ -17,6 +19,7 @@ public class PerformanceEndpoint {
 
     private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
+    @Secured("ROLE_USER")
     @PostMapping
     public OrderDto buySeats(CartDto cartDto, int id, UserDto userDto) {
         return this.performanceService.buyTickets(cartDto, id, userDto);
