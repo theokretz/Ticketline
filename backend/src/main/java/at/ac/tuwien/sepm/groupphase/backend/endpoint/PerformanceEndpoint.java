9554e8ef536f71f139b68c02f6cd97452ac4245f
Theo Kretz <e52004057@student.tuwien.ac.at>
2023-05-10 20:25:47 +0200

    added RequestMapping Annotation #64

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/PerformanceEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/PerformanceEndpoint.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -22,6 +23,7 @@ import java.lang.invoke.MethodHandles;
 
 
 @RestController
+@RequestMapping(value = "/api/v1/performances")
 public class PerformanceEndpoint {
     private final PerformanceService performanceService;
 
