39b7aea314b347be140aed05e5db21460152b568
Hermann Stoss <hermann.stoss@tuwien.ac.at>
2023-05-17 22:36:34 +0000

    Stages optimization

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/PerformanceRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/PerformanceRepository.java
@@ -1,7 +1,6 @@
 package at.ac.tuwien.sepm.groupphase.backend.repository;
 
 import at.ac.tuwien.sepm.groupphase.backend.entity.Performance;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -11,24 +10,7 @@ import java.util.Optional;
  * The interface Performance repository.
  */
 public interface PerformanceRepository extends JpaRepository<Performance, Integer> {
-    /*
-     * Find a single performance entry by id.
-     * with eagerLoading for hall.location, performanceSectors,
-     * performanceSectors.sector.seats,performanceSectors.sector.seats,
-     * event, tickets, tickets.reservation
-     *
-     * @param id the id of the performance entry
-     * @return the performance entry
-     */
-    @EntityGraph(attributePaths = {
-        "hall.location",
-        "performanceSectors",
-        "performanceSectors.sector",
-        "performanceSectors.sector.seats",
-        "event",
-        "tickets",
-        "tickets.reservation"})
-    Optional<Performance> findPerformanceById(Integer id);
 
+    Optional<Performance> findAllPerformancesById(Integer id);
 }
 
